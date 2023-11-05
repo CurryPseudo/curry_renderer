2c7e0e39f796642b0774567bc86707f90de5134f
currypseudo <currypseudo@gmail.com>
2023-11-05 19:17:08 +0800

    fix: bench

--- renderer/benches/cpu_renderer.rs
+++ renderer/benches/cpu_renderer.rs
@@ -1,6 +1,6 @@
 use criterion::*;
-use triangles_2d::SyncCpuRenderer;
-use triangles_2d::*;
+use curry_renderer::SyncCpuRenderer;
+use curry_renderer::*;
 fn render_benchmark(c: &mut Criterion) {
     let mut cpu_renderer = SyncCpuRenderer::default();
     cpu_renderer.resize_frame(UVec2::new(1920, 1080));
